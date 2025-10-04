bf20d8666a2dc15d55097f5a22919bdcca604f98
Sofi Lahham <lahhamsofia@gmail.com>
2025-10-04 20:53:12 -0300

    Update __init__.py

--- Streaming/__init__.py
+++ Streaming/__init__.py
@@ -1,2 +1,9 @@
 # pacote principal do projeto (Streaming de Música)
 
+from .menu import Menu
+from .arquivo_midia import ArquivoDeMidia
+from .musica import Musica
+from .podcast import Podcast
+from .playlist import Playlist
+from .usuario import Usuario
+from .analises import Analises
